9d59eef99967e444ff6db8f2449c16dc35f459aa
Matt Ashby <matthew.ashby@ntu.ac.uk>
2020-12-02 18:13:39 +0000

    Add Downtown Atlanta homicide data

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# crimemapping
+<!-- badges: start -->
+[![Lifecycle: experimental](https://img.shields.io/badge/lifecycle-experimental-orange.svg)](https://www.tidyverse.org/lifecycle/#experimental)
+<!-- badges: end -->
+
+# crimemapping package
+
 Tutorials for crime mapping in R
---
+++ inst/tutorials/prep/code/create_downtown_homicides_dataset.R
@@ -0,0 +1,31 @@
+library("sf")
+library("tidyverse")
+
+# download crime data
+download.file(
+  "https://www.atlantapd.org/Home/ShowDocument?id=3051",
+  destfile = str_glue("{tempdir()}/atlanta_crime.zip")
+)
+
+# unzip crime data
+unzip(str_glue("{tempdir()}/atlanta_crime.zip"), exdir = tempdir())
+
+# load crime data
+crimes <- str_glue("{tempdir()}/COBRA-2009-2019.csv") %>%
+  read_csv() %>%
+  janitor::clean_names()
+
+# load neighbourhood data
+nbhds <- read_sf("https://opendata.arcgis.com/datasets/d6298dee8938464294d3f49d473bcf15_196.geojson") %>%
+  janitor::clean_names() %>%
+  filter(neighborho == "Castleberry Hill, Downtown")
+
+# get downtown homicides
+crimes %>%
+  filter(
+    ucr_literal == "HOMICIDE",
+    occur_date >= as.Date("2019-01-01"),
+    neighborhood == "Downtown"
+  ) %>%
+  st_as_sf(coords = c("longitude", "latitude"), crs = 4326) %>%
+  st_write("../data/")
