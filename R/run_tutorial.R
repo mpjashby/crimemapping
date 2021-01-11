6a46b18dc13ac8da0e4bc6a515c53b71cb066637
Matt Ashby <matthew.ashby@ntu.ac.uk>
2021-01-11 00:11:37 +0000

    Export tutorial function

--- R/run_tutorial.R
+++ R/run_tutorial.R
@@ -3,9 +3,11 @@
 #' This function runs a crime mapping tutorial.
 #'
 #' @param name Name of the tutorial to be loaded
+#'
+#' @export
 
 
-crimemapping_tutorial <- function(name = NULL) {
+tutorial <- function(name = NULL) {
 
   learnr::run_tutorial(name = name, package = "crimemapping")
 
