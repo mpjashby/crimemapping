c8a3ce5fbd1a62fd3818bb21dfb0bbd0b124dd56
Matt Ashby <matthew.ashby@ntu.ac.uk>
2021-01-17 00:14:57 +0000

    Add Vancouver theft data

---
+++ inst/tutorials/prep/code/create_vancouver_crime_dataset.R
@@ -0,0 +1,14 @@
+# load packages
+library(tidyverse)
+
+# load data
+unzip(
+  here::here("inst/tutorials/04_hotspots_1/vancouver_crimedata_csv_all_years.zip"),
+  exdir = str_glue("{tempdir()}/vancouver_crime_data/")
+)
+
+crimes <- read_csv(str_glue("{tempdir()}/vancouver_crime_data/crimedata_csv_all_years.csv"))
+
+crimes %>%
+  filter(YEAR %in% 2020, str_detect(TYPE, "Theft")) %>%
+  write_csv(here::here("inst/extdata/vancouver_thefts.csv.gz"))
