b4861f601fb9c2f5b049f37f7a4ae4b0844e2685
Matt Ashby <matthew.ashby@ntu.ac.uk>
2021-01-26 05:06:13 +0000

    Add week 3 tutorials

--- inst/tutorials/prep/code/create_bronx_shootings.R
+++ inst/tutorials/prep/code/create_bronx_shootings.R
@@ -1,3 +1,10 @@
+library(lubridate)
 library(tidyverse)
 
-shootings <- read_csv("NYPD_Shooting_Incident_Data__Historic_.csv")
+read_csv("inst/tutorials/prep/code/NYPD_Shooting_Incident_Data__Historic_.csv") %>%
+  janitor::clean_names() %>%
+  mutate(occur_date = mdy(occur_date)) %>%
+  filter(occur_date >= ymd("2019-01-01"), boro == "BRONX") %>%
+  select(incident_key, occur_date, murder = statistical_murder_flag, longitude, latitude) %>%
+  write_csv("inst/extdata/bronx_shootings.csv")
+
