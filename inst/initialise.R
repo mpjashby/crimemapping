7bb4e522c91def90877f724cd6fedb0c5f5d676a
mpjashby <mpjashby@gmail.com>
2022-01-06 23:48:11 +0000

    Remove taught packages from Imports/Suggests

--- inst/initialise.R
+++ inst/initialise.R
@@ -1,28 +1,38 @@
 # print initial message
 message(
-  "\n\n-------------------------\n\nCRIME MAPPING COURSE SETUP\n\nThis code ",
-  "sets up RStudio for the UCL Crime Mapping module (SECU0005). More ",
-  "specifically, it installs R packages that you will need for this module. ",
-  "The code may take a few minutes to run and R will print messages for each ",
-  "package being installed. If you have any questions, please ask them on the ",
-  "module Q&A forum on Moodle at:\n\n    ",
-  "https://moodle.ucl.ac.uk/mod/hsuforum/view.php?id=1753357",
-  "\n\n(be warned that R may have wrapped this URL by inserting spaces into ",
-  "it).\n\n-------------------------\n\n"
+  "\n\n-------------------------\n\n",
+  "CRIME MAPPING COURSE SETUP\n\n",
+  "This code sets up RStudio for the UCL Crime Mapping module (SECU0005). More\n",
+  "specifically, it installs R packages that you will need for this module. The\n",
+  "code may take a few minutes to run and R will print messages for each\n",
+  "package being installed. If you have any questions, please ask them on the\n",
+  "module Q&A forum on Moodle at:\n\n",
+  "    https://moodle.ucl.ac.uk/mod/hsuforum/view.php?id=1753357\n\n",
+  "(be warned that R may have wrapped this URL by inserting spaces into it).\n\n",
+  "Another message will appear when the process is complete.\n\n",
+  "-------------------------\n\n"
 )
 
 # load packages
-install.packages(c("tidyverse", "crimedata", "learnr", "remotes"), type = "source", quiet = TRUE)
-remotes::install_github("mpjashby/crimemapping", upgrade = "always", quiet = TRUE)
+install.packages(
+  c("tidyverse", "crimedata", "learnr", "remotes"),
+  type = "source",
+  verbose = FALSE
+)
+remotes::install_github(
+  "mpjashby/crimemapping",
+  upgrade = "always"
+)
 
 # print final message
 message(
-  "\n\n-------------------------\n\nCRIME MAPPING COURSE SETUP COMPLETED\n\n",
-  "If any errors occurred during setup, please post the complete error ",
-  "message on the module Q&A forum on Moodle at:\n\n    ",
-  "https://moodle.ucl.ac.uk/mod/hsuforum/view.php?id=1753357",
-  "\n\n(be warned that R may have wrapped this URL by inserting spaces into ",
-  "it).\n\n-------------------------\n\n"
+  "\n\n-------------------------\n\n",
+  "CRIME MAPPING COURSE SETUP COMPLETED\n\n",
+  "If any errors or warnings occurred during setup, please post the complete \n",
+  "error message on the module Q&A forum on Moodle at:\n\n",
+  "    https://moodle.ucl.ac.uk/mod/hsuforum/view.php?id=1753357\n\n",
+  "(be warned that R may have wrapped this URL by inserting spaces into it).\n\n",
+  "-------------------------\n\n"
 )
 
 # restart R to load tutorials into Tutorial tab
