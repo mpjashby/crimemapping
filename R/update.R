9f878e1b015ae14e674a3312624f13f027c59e29
mpjashby <mpjashby@gmail.com>
2022-01-20 12:27:43 +0000

    Add function to update package

---
+++ R/update.R
@@ -0,0 +1,31 @@
+#' Update tutorials
+#'
+#' This function updates the crime mapping tutorials.
+#'
+#' @param ... Further arguments passed to \code{\link[remotes]{install_github}}.
+#'
+#' @export
+
+update <- function (...) {
+
+  message(
+    "\n\n-------------------------\n\n",
+    "UPDATING CRIME MAPPING COURSE\n\n",
+    "R will now update the Crime Mapping course tutorials. This may take\n",
+    "a few minutes. If you are asked whether you want to update any other\n",
+    "packages, choose to 'update CRAN packages only'. If you are asked\n",
+    "'Do you want to install from sources the package which needs \n",
+    "compilation?', choose 'no'.\n\n",
+    "Another message will appear when the process is complete.\n\n",
+    "-------------------------\n\n"
+  )
+
+  remotes::install_github("mpjashby/crimemapping", upgrade = "always", ...)
+
+  message(
+    "\n\n-------------------------\n\n",
+    "CRIME MAPPING COURSE UPDATE COMPLETED\n\n",
+    "-------------------------\n\n"
+  )
+
+}
