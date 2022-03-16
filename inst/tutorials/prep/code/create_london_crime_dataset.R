999863e62d0e952cac02ee8ba36e6388fee5d459
mpjashby <mpjashby@gmail.com>
2022-03-16 13:16:36 +0000

    Add 2021 London crime data for assessment

--- inst/tutorials/prep/code/create_london_crime_dataset.R
+++ inst/tutorials/prep/code/create_london_crime_dataset.R
@@ -2,10 +2,17 @@ library(lubridate)
 library(tidyverse)
 
 temp_dir <- str_glue("{tempdir()}/crime_data2")
-unzip(
-  zipfile = here::here("inst/tutorials/prep/code/london_crime_data_raw.zip"),
-  exdir = temp_dir
+temp_file <- tempfile(fileext = ".zip")
+# THIS URL WILL NO LONGER WORK AND SHOULD BE REPLACED WITH A NEW ONE FROM
+# https://data.police.uk/data/ WITH THE FOLLOWING OPTIONS:
+#   * a single calendar year
+#   * BTP, CoLP and MPS
+#   * crimes only
+download.file(
+  url = "https://policeuk-data.s3.amazonaws.com/download/8879e5677478641fc1c194161b4f6f792d048be6.zip",
+  destfile = temp_file
 )
+unzip(zipfile = temp_file, exdir = temp_dir)
 
 crimes <- temp_dir %>%
   dir(pattern = ".csv$", full.names = TRUE, recursive = TRUE) %>%
@@ -14,7 +21,7 @@ crimes <- temp_dir %>%
 london_crimes <- crimes %>%
   janitor::clean_names() %>%
   separate(month, into = c("year", "month"), sep = "-", convert = TRUE) %>%
-  filter(year == 2020) %>%
+  filter(year == 2021) %>%
   mutate(
     across(c(longitude, latitude), round, digits = 4),
     borough = str_remove(str_remove(lsoa_name, "\\s\\w{4}$"), " upon Thames$"),
@@ -30,8 +37,11 @@ london_crimes <- crimes %>%
       str_remove("^On or near ") %>%
       str_replace(coll(" (Dlr)"), " DLR Station") %>%
       str_replace(coll(" (Lu Station)"), " LU Station") %>%
-      str_replace(coll(" (Station)"), " Rail Station")
+      str_replace(coll(" (Station)"), " Rail Station"),
+    month = month(make_date(month = month), label = TRUE),
+    random = runif(n())
   ) %>%
+  arrange(month, random) %>%
   filter(
     borough %in% c(
       "Westminster", "Tower Hamlets", "Camden", "Southwark", "Lambeth",
@@ -47,4 +57,4 @@ london_crimes <- crimes %>%
     month, type = crime_type, location, lsoa = lsoa_code, borough, longitude,
     latitude
   ) %>%
-  write_csv(here::here("inst/extdata/london_crimes_2020.csv.gz"))
+  write_csv(here::here("inst/extdata/london_crimes.csv.gz"))
